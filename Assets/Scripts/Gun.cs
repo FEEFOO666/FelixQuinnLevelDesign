3d54a9ad6f82a60c7536871c6b5ade68a27ef076
Felix <fbquinn7@gmail.com>
2021-04-20 14:14:45 +1000

    Project initialisation
    Set up repo with existing project

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    GameObject bulletSpawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletSpawnPoint = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+        }
+    }
+}
